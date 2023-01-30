5a1ca176eedeefef9355be062891dbe8fb7aba8a
kursad <varolkursat@gmail.com>
2023-01-30 15:45:51 -0500

    .

--- src/funFolder/FirstClass.java
+++ src/funFolder/FirstClass.java
@@ -8,6 +8,8 @@ public class FirstClass {
         System.out.println("h1");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("Hello4");
+
     }
 
 }
