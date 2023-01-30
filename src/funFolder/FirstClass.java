a88924acd110c815781e0cc9467913da3c481363
katpvc <katpvc@aol.com>
2023-01-29 19:15:37 -0500

    changed the line

--- src/funFolder/FirstClass.java
+++ src/funFolder/FirstClass.java
@@ -2,10 +2,13 @@ package funFolder;
 
 public class FirstClass {
     public static void main(String[] args) {
-        //mmmmmmmm
+
+
         System.out.println("hello1");
+        System.out.println("Bahruz");
         System.out.println("hello2");
-        System.out.println("hello3");
+
+
     }
 
 }
