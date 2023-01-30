8ec975ba69b67fe0dc2314cdbe53bcb6f602523a
Bahruz Gadirov <qedirovbehruz@gmail.com>
2023-01-29 19:01:12 -0500

    second commit b

--- src/funFolder/FirstClass.java
+++ src/funFolder/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         //mmmmmmmm
         System.out.println("hello1");
-        System.out.println("hello2");
+        System.out.println("h1");
 
     }
 
