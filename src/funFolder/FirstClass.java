75e1bfa065dd3aabf43248bd8fbb02ba3aa76f13
Fazi_Cydeo <fazi.6246@mail.ru>
2023-01-30 19:16:35 -0800

    added note

--- src/funFolder/FirstClass.java
+++ src/funFolder/FirstClass.java
@@ -8,6 +8,7 @@ public class FirstClass {
         System.out.println("h1");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("Fazi");
     }
 
 }
